90c7529d7311222c3bad43f643ecbbbe084a05e6
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-02-04 15:57:27 -0500

    Split out into two hyperlinks.

--- client/components/listings/listingCard/listingCard.jsx
+++ client/components/listings/listingCard/listingCard.jsx
@@ -2,28 +2,30 @@ ListingCard = React.createClass({
   render() {
     return (
       <div className="card">
-        <div className="card-thumbnail">
-          <img src={this.props.cardThumbnail} alt="" className="img-responsive"/>
-        </div>
-        <h5 className="card-price no-margin text-center">{this.props.price}</h5>
-        <div className="card-content">
-          <h5 className="title no-margin">{this.props.name}</h5>
-          <div className="city subtle-text"><i className="material-icons md-18">place</i> {this.props.city}</div>
-        </div>
-        <div className="card-owner">
-          <div className="media">
-            <div className="media-left">
-              <a href="#">
+        <a href="#">
+          <div className="card-thumbnail">
+            <img src={this.props.cardThumbnail} alt="" className="img-responsive"/>
+          </div>
+          <h5 className="card-price no-margin text-center">{this.props.price}</h5>
+          <div className="card-content">
+            <h5 className="title no-margin">{this.props.name}</h5>
+            <div className="city subtle-text"><i className="material-icons md-18">place</i> {this.props.city}</div>
+          </div>
+        </a>
+        <a href="#">
+          <div className="card-owner">
+            <div className="media">
+              <div className="media-left">
                 <img className="media-object img-circle" src={this.props.sellerAvatar} alt="..." />
-              </a>
-            </div>
-            <div className="media-body">
-              <div className="username">{this.props.sellerName}</div>
-              <div className="timestamp subtle-text">hace 2 minutos</div>
+              </div>
+              <div className="media-body">
+                <div className="username">{this.props.sellerName}</div>
+                <div className="timestamp subtle-text">hace 2 minutos</div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>     
+        </a>
+      </div>
     );
   }
 
