a21abd9f93305e00a13fdfb90f8a076b4c738209
Daniel Tapia <dan.tg@outlook.com>
2016-02-04 19:59:50 -0500

    Moved featured listings to a component.

---
+++ client/components/listings/featuredListings/featuredListings.jsx
@@ -0,0 +1,30 @@
+FeaturedListings = React.createClass({
+  render() {
+    return (
+      <div>
+        <h4 className="discover-heading subtle-text">{this.props.categoryName} <a href="#">ver más</a></h4>
+        <div className="row card-row">
+          <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
+            <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$250"} name={"PS4 500GB Nuevo"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
+          </div>
+
+          <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
+            <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$4,000"} name={"Kia Optima 2007"} city={"La Paz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Oscar N."} />
+          </div>
+
+          <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
+            <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$24,000"} name={"Vendo casa grande!"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
+          </div>
+
+          <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
+            <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$500"} name={"Jordan 10 OVO"} city={"Cochabamba"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
+          </div>
+          <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
+            <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$250"} name={"PS4 500GB Nuevo"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+});
--- client/views/home/home.jsx
+++ client/views/home/home.jsx
@@ -8,7 +8,6 @@ Home = React.createClass({
           <div className="box">
             <div className="box-content">Hi</div>
           </div>
-          
           <div className="row">
             <div className="visible-lg col-lg-2">
               <div className="box category-sidebar">
@@ -22,71 +21,15 @@ Home = React.createClass({
             </div>
 
             <div className="col-md-12 col-lg-10">
-              <h4 className="discover-heading subtle-text">Autos y Vehiculos <a href="#">ver más</a></h4>
-              <div className="row card-row">
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$250"} name={"PS4 500GB Nuevo"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
+              <FeaturedListings categoryName={"Autos y Vehiculos"} />
 
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$4,000"} name={"Kia Optima 2007"} city={"La Paz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Oscar N."} />
-                </div>
+              <hr className="home-separator"/>
 
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$24,000"} name={"Vendo casa grande!"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
+              <FeaturedListings categoryName={"Bienes Raices"}  />
 
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$500"} name={"Jordan 10 OVO"} city={"Cochabamba"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$250"} name={"PS4 500GB Nuevo"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-              </div>
+              <hr className="home-separator"/>
 
-              <h4 className="discover-heading subtle-text">Bienes Raices <a href="#">ver más</a></h4>
-              <div className="row card-row">
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$250"} name={"PS4 500GB Nuevo"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$4,000"} name={"Kia Optima 2007"} city={"La Paz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Oscar N."} />
-                </div>
-
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$24,000"} name={"Vendo casa grande!"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$500"} name={"Jordan 10 OVO"} city={"Cochabamba"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$250"} name={"PS4 500GB Nuevo"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-              </div>
-
-              <h4 className="discover-heading subtle-text">Celulares y Accesorios <a href="#">ver más</a></h4>
-              <div className="row card-row">
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$250"} name={"PS4 500GB Nuevo"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$4,000"} name={"Kia Optima 2007"} city={"La Paz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Oscar N."} />
-                </div>
-
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$24,000"} name={"Vendo casa grande!"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$500"} name={"Jordan 10 OVO"} city={"Cochabamba"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-                <div className="col-lg-5ths col-md-3 col-sm-4 col-xs-6">
-                  <ListingCard cardThumbnail={"http://placehold.it/300x300"} price={"$250"} name={"PS4 500GB Nuevo"} city={"Santa Cruz"} sellerAvatar={"http://lorempixel.com/32/32"} sellerName={"Daniel T."} />
-                </div>
-              </div>
+              <FeaturedListings categoryName={"Celulares y Accesorios"} />
             </div>
           </div>
         </div>
