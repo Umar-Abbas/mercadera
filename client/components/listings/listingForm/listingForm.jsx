e8df2dd93a89703fa505624e612e3b9eec4373f8
Daniel Tapia <dan.tg@outlook.com>
2016-02-04 21:18:33 -0500

    Fixed create listing form view.

--- client/components/listings/listingForm/listingForm.jsx
+++ client/components/listings/listingForm/listingForm.jsx
@@ -5,12 +5,19 @@ ListingForm = React.createClass({
 
   render() {
     return (
-      <form className="form-horizontal">
-        <TextInput name={"name"} label={"Nombre:"} placeholder={"Escribe un nombre para tu anuncio. Corto y sencillo!"} errors={[]} onChange={this.handleChange} />
+      <form className="form-horizontal listing-form">
+        <div className="form-group upload-group">
+          <div className="upload-box text-center">
+            <i className="material-icons md-36">add_a_photo</i>
+            <h5 className="no-margin">Haz clic o arrastra para subir imagenes</h5>
+          </div>
+        </div>
 
-        <TextInput name={"name"} label={"Nombre:"} placeholder={"Escribe un nombre para tu anuncio. Corto y sencillo!"} errors={["can't be empty.", "too long"]} onChange={this.handleChange} />
+        <hr/>
 
         <SelectInput name={"category"} label={"Categoria:"} errors={[]} onChange={this.handleChange} />
+
+        <TextInput name={"name"} label={"Titulo:"} placeholder={"Escribe un nombre para tu anuncio. Corto y sencillo!"} errors={[]} onChange={this.handleChange} />
 
         <TextAreaInput name={"description"} label={"Descripción:"} rows={"3"} errors={[]} onChange={this.handleChange} />
 
@@ -18,61 +25,9 @@ ListingForm = React.createClass({
 
         <AmountInput name={"price"} label={"Precio:"} placeholder={"0"} errors={[]} onChange={this.handleChange} />
 
-        <h1>These below are just static HTML, need to move to component.</h1>
-
-        <div className="form-group">
-          <label className="col-sm-2 control-label">Imagenes</label>
-          <div className="col-sm-10">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="upload-box"><a href="#" className="btn btn-default">Upload Image</a></div>
-              </div>
-              <div className="col-md-4">
-                <div className="upload-box"><a href="#" className="btn btn-default">Upload Image</a></div>
-              </div>
-              <div className="col-md-4">
-                <div className="upload-box"><a href="#" className="btn btn-default">Upload Image</a></div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <hr/>
-
-        <div className="form-group">
-          <label for="inputPassword3" className="col-sm-2 control-label">Categoria</label>
-          <div className="col-sm-10">
-            <select className="form-control">
-              <option>Elige una categoria para tu anuncio</option>
-            </select>
-          </div>
-        </div>
-
-        <TextAreaInput name={"description"} label={"Descripción:"} rows={"3"} errors={[]} onChange={this.handleChange} />
-
-        <div className="form-group">
-          <label for="inputPassword3" className="col-sm-2 control-label">Ubicación</label>
-          <div className="col-sm-10">
-            <select className="form-control">
-              <option>Elige una ciudad</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label for="inputPassword3" className="col-sm-2 control-label">Precio</label>
-          <div className="col-sm-10">
-            <div className="input-group">
-              <span className="input-group-addon">$</span>
-              <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" />
-              <span className="input-group-addon">.00</span>
-            </div>
-          </div>
-        </div>
-
         <div className="form-group">
           <div className="col-sm-offset-2 col-sm-10">
-            <button type="submit" className="btn btn-primary">Publicar Anuncio</button>
+            <button type="submit" className="btn btn-success">Publicar Anuncio</button>
           </div>
         </div>
       </form>
--- client/views/createListing/createListing.jsx
+++ client/views/createListing/createListing.jsx
@@ -4,11 +4,14 @@ CreateListing = React.createClass({
     return (
       <div>
         <div className="container">
-          <div className="box">
-            <div className="box-heading text-center text-uppercase">Publica tu anuncio</div>
-
-            <div className="box-padding">
-              <ListingForm listing={{}} />
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2 mobile-padding">
+              <div className="box">
+                <div className="box-heading text-uppercase text-center">Publica tu anuncio</div>
+                <div className="box-padding">
+                  <ListingForm />
+                </div>
+              </div>
             </div>
           </div>
         </div>
